f125d7098aa7ef10a71b3eed95bb804dc20a902e
togashidm <denisio.togashi@intel.com>
2025-06-13 09:41:57 +0000

    fix: adjust text to avoid substring mismatch

--- tests/robustness-test/cluster_orch_robustness_test.go
+++ tests/robustness-test/cluster_orch_robustness_test.go
@@ -230,13 +230,13 @@ var _ = Describe("Cluster Orch Robustness tests", Ordered, Label(utils.ClusterOr
 		Expect(err).NotTo(HaveOccurred())
 		Expect(resp.StatusCode).To(Equal(http.StatusOK))
 
-		By("Verifying the providerStatus.message is 'ConnectAgentDisconnected'")
+		By("Verifying the providerStatus.message is 'connect agent is disconnected'")
 		providerStatus, ok := clusterInfo["providerStatus"].(map[string]interface{})
 		Expect(ok).To(BeTrue(), "providerStatus field is missing or not a map")
 
 		message, ok := providerStatus["message"].(string)
 		Expect(ok).To(BeTrue(), "message field is missing or not a string")
-		Expect(message).To(ContainSubstring("ConnectAgentDisconnected"), "providerStatus.message does not contain 'ConnectAgentDisconnected'")
+		Expect(message).To(ContainSubstring("connect agent is disconnected"), "providerStatus.message does not contain 'connect agent is disconnected'")
 
 	})
 
