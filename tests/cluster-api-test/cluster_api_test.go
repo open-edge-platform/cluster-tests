7c5ed51c8a6c78a88e15dd2b6605d8370c8213c5
togashidm <denisio.togashi@intel.com>
2026-02-18 19:44:05 +0000

    test: increase ven readiness timeouts

--- tests/cluster-api-test/cluster_api_test.go
+++ tests/cluster-api-test/cluster_api_test.go
@@ -48,7 +48,7 @@ func clusterReadinessTimeout() time.Duration {
 
 	// vEN provisioning can take longer than the in-kind ENiC flow.
 	if utils.GetEdgeNodeProvider() == utils.EdgeNodeProviderVEN {
-		return 5 * time.Minute
+		return 10 * time.Minute
 	}
 	return DefaultClusterReadinessTimeout
 }
@@ -60,7 +60,7 @@ func podReadinessTimeout() time.Duration {
 		}
 	}
 	if utils.GetEdgeNodeProvider() == utils.EdgeNodeProviderVEN {
-		return 5 * time.Minute
+		return 10 * time.Minute
 	}
 	return DefaultPodReadinessTimeout
 }
